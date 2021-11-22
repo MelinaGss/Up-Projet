717cd919a5fc2cbe676fbd193a723d130c1a25cb
Mélina Goussios <mgoussios@orange.fr>
2021-11-22 17:38:33 +0100

    Add new piza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'my pizza': {
+	'ingredients': {'mozzarella': 3, 'garlic': 2},
+	'price': 5,
+    }	
 }
